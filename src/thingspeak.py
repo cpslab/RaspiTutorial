7bb765c716fe3099577264b345b5d82f8ab8e6aa
dattiren <ren@cps.im.dendai.ac.jp>
2017-08-29 00:19:40 +0900

    Update thingspeak.py

--- src/thingspeak.py
+++ src/thingspeak.py
@@ -3,22 +3,20 @@ import urllib.parse
 import urllib.request
 import time
 
-sleep = 7
+sleep = 15
 key = "Write_API_KEY" 
 sense = SenseHat()
+
 def send_data():
-    while True:
-        humidity = sense.get_humidity()
-        params = urllib.parse.urlencode({'api_key': key, 'field1': humidity})
-        url = 'https://api.thingspeak.com/update?' + params
-        try:
-            f = urllib.request.urlopen(url)
-        except:
-            print("connection failed")
-        break
+    humidity = sense.get_humidity()
+    params = urllib.parse.urlencode({'api_key': key, 'field1': humidity})
+    url = 'https://api.thingspeak.com/update?' + params
+    try:
+        f = urllib.request.urlopen(url)
+    except:
+        print("connection failed")
 
 if __name__ == '__main__':
     while True:
         send_data()
         time.sleep(sleep)
-
